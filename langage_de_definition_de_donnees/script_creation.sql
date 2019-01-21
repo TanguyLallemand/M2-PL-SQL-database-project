8eadb1bc230a878c1f7f5732ed56a0764953b89a
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-01-21 20:42:38 +0100

    add emprunt table

--- langage_de_definition_de_donnees/script_creation.sql
+++ langage_de_definition_de_donnees/script_creation.sql
@@ -5,10 +5,9 @@ CREATE TABLE OUVRAGE
 (
 	ISBN VARCHAR2(13) NOT NULL, -- Use last version of ISBN named GENCOD and composed by 13 char
 	Titre VARCHAR2(100) NOT NULL,
-    Genre VARCHAR2(10) NOT NULL,
-	Auteur VARCHAR2(20), -- can be null because not always known
-	Editeur VARCHAR2(20), -- can be null because not always known
-	CONSTRAINT Genre_Libelle FOREIGN KEY(Genre) REFERENCES Libelle_genre(Genre),
+    Genre VARCHAR2(20) NOT NULL,
+	Auteur VARCHAR2(40), -- can be null because data are not always known
+	Editeur VARCHAR2(40), -- can be null because data are not always known
 	CONSTRAINT pk_Ouvrage PRIMARY KEY (ISBN)
 );
 
@@ -23,13 +22,22 @@ create table EXEMPLAIRE
 
 create table MEMBRE
 (
-	ID NUMBER(6),
-	Nom VARCHAR2(20) NOT NULL,
-	Prenom VARCHAR2(20) NOT NULL,
-	Adresse VARCHAR2(30) NOT NULL,
-	Telephone VARCHAR2(10),
+	ID_membre NUMBER(6) NOT NULL,
+	Nom VARCHAR2(40) NOT NULL,
+	Prenom VARCHAR2(40) NOT NULL,
+	Adresse VARCHAR2(50) NOT NULL,
+	Telephone VARCHAR2(10) NOT NULL,
 	Date_adhesion DATE NOT NULL,
 	Duree NUMBER(2) CHECK( Duree IN(1, 3, 6, 12)) NOT NULL,
-	CONSTRAINT pk_Membre PRIMARY KEY (ID),
+	CONSTRAINT pk_Membre PRIMARY KEY (ID_membre),
 	CONSTRAINT Tel_unique UNIQUE (Telephone)
+);
+
+create table EMPRUNTS
+(
+	ID_emprunt NUMBER(6) NOT NULL,
+	ID_membre NUMBER(6) NOT NULL,
+	Cree_le DATE NOT NULL,
+	CONSTRAINT ID_membre_Membre FOREIGN KEY(ID_membre) references Membre (ID_membre),
+	CONSTRAINT pk_emprunt PRIMARY KEY (ID_emprunt)
 );
