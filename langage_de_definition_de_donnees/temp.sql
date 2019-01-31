3f04bb10aa1fb1c8049540b348ab89ec374dc747
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-01-31 17:27:10 +0100

    progress in question 5-5, not finished yet

--- langage_de_definition_de_donnees/temp.sql
+++ langage_de_definition_de_donnees/temp.sql
@@ -1,21 +1,21 @@
 -- PARTIE V Q1
 CREATE OR REPLACE FUNCTION Finvalidite (Num_adhe IN NUMBER)
-  RETURN DATE
-  IS
-  V_datedeb Membre.Date_adhesion%TYPE;
-  V_duree Membre.Duree%TYPE;
-  V_finval DATE;
-  BEGIN
+    RETURN DATE
+    IS
+    V_datedeb Membre.Date_adhesion%TYPE;
+    V_duree Membre.Duree%TYPE;
+    V_finval DATE;
+BEGIN
     SELECT Duree INTO V_duree
     FROM Membre
-    WHERE Id_membre = Numadhe;
+    WHERE Id_membre = Num_adhe;
     SELECT Date_adhesion INTO V_datedeb
     FROM Membre
-    WHERE Id_membre = Numadhe;
+    WHERE Id_membre = Num_adhe;
     V_finval := Add_months(V_datedeb, V_duree);
     Dbms_output.Put_line(V_finval);
     RETURN V_finval;
-  END;
+END;
 /
 --CODE POUR TESTER LA FONCTION
 --DECLARE
@@ -30,13 +30,13 @@ CREATE OR REPLACE FUNCTION Finvalidite (Num_adhe IN NUMBER)
 CREATE OR REPLACE FUNCTION Adhesionajour (Num_adhe IN NUMBER)
   RETURN BOOLEAN
 AS
-    BEGIN
-        IF (Finvalidite(Numadhe) >= Sysdate()) THEN
-          RETURN TRUE;
-        ELSE
-          RETURN FALSE;
-        END IF;
-    END;
+BEGIN
+    IF (Finvalidite(Num_adhe) >= Sysdate()) THEN
+      RETURN TRUE;
+    ELSE
+      RETURN FALSE;
+    END IF;
+END;
 /
 --CODE POUR TESTER LA FONCTION
 -- DECLARE
@@ -48,9 +48,35 @@ AS
 -- /
 
 -- PARTIE V Q3
-CREATE OR REPLACE FUNCTION Retourexemplaire (Num_isbn IN VARCHAR2, Num_exemplaire NUMBER)
+CREATE OR REPLACE PROCEDURE Retourexemplaire (Num_isbn IN VARCHAR2, Num_exemplaire NUMBER)
 AS
-    BEGIN
-        UPDATE Details SET Date_retour=Sysdate
-        WHERE Date_retour IS NULL AND Isbn=Num_isbn AND Numero_exemplaire = Num_exemplaire;
-    END;
+BEGIN
+    UPDATE Details SET Date_retour=Sysdate
+    WHERE Date_retour IS NULL AND Isbn=Num_isbn AND Numero_exemplaire = Num_exemplaire;
+END;
+/
+
+-- PARTIE V Q4
+CREATE OR REPLACE PROCEDURE Purgemembres AS
+BEGIN
+
+END;
+/
+
+-- PARTIE V Q5
+CREATE OR REPLACE FUNCTION Mesureactivite (V_periode IN number)
+RETURN number IS
+    CURSOR C_activite(V_mois IN number) IS
+        SELECT Id_membre, Count(*)
+        FROM Emprunts, Details
+        WHERE Details.Id_emprunt=Emprunts.Numero_exemplaire AND months_between(Sysdate, Cree_le) < V_mois
+        GROUP BY Id_membre
+        ORDER BY 2 DESC;
+    V_membre C_activite%Rowtype;
+BEGIN
+    OPEN C_activite(V_periode);
+    FETCH C_activite INTO V_membre;
+    CLOSE C_activite;
+    RETURN V_membre.Id_membre;
+END;
+/
