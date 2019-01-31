291cebe1297aade11e01c15a84bb75b3d031c098
LouisonF <fresnaislouison@gmail.com>
2019-01-31 17:06:00 +0100

    update the book update, privileges problems

--- langage_de_definition_de_donnees/update_book_quality.sql
+++ langage_de_definition_de_donnees/update_book_quality.sql
@@ -8,31 +8,36 @@
 
 /* WORK IN PROGRESS, NEED TO CREATE A PACKAGE APPARENTLY)*/
 /*WORKING ON CASES*/
-
+--TRIGGER
+  CREATE OR REPLACE TRIGGER book_update
+       AFTER INSERT ON DETAILS --OPTIONNAL: IF INSERTION IN DETAILS, ACTIVATE TRIGGER. HIGH RESSOURCE COST
+     --BEFORE LOGOFF ON DATABASE --ACTIVATE TRIGGER WHEN THE USER LOGOFF TO LEAVE HIS WORK INSUFFICIENT PRIVILEGES AGAIN
 --CURSEUR
 DECLARE
   CURSOR liste_exemplaire IS
    SELECT * FROM EXEMPLAIRE WHERE ISBN = current;
    champ_isbn EXEMPLAIRE.ISBN%TYPE;
    champ_num_ex EXEMPLAIRE.Numero_exemplaire%TYPE;
-
 BEGIN
+    BEGIN
 
- OPEN liste_exemplaire;
- FOR LIGNE IN liste_exemplaire LOOP
-   number_uses = utils.number_uses_book(liste_exemplaire.ISBN,liste_exemplaire.Numero_exemplaire);
-   CASE
-     WHEN number_uses<11 THEN UPDATE EXEMPLAIRE SET Etat = ''NEUF'' WHERE ISBN = liste_exemplaire.ISBN, Numero_exemplaire = liste_exemplaire.Numero_exemplaire;
-     WHEN number_uses>11 AND number_uses<25 THEN UPDATE EXEMPLAIRE SET Etat = ''BON'' WHERE ISBN = liste_exemplaire.ISBN, Numero_exemplaire = liste_exemplaire.Numero_exemplaire;
-     WHEN number_uses>25 AND number_uses<60 THEN UPDATE EXEMPLAIRE SET Etat = ''MOYEN'' WHERE ISBN = liste_exemplaire.ISBN, Numero_exemplaire = liste_exemplaire.Numero_exemplaire;
-     WHEN number_uses>60 THEN DELETE EXEMPLAIRE WHERE ISBN = liste_exemplaire.ISBN, Numero_exemplaire = liste_exemplaire.Numero_exemplaire;
-     ELSE ''12''
- END as liste_exemplaire
- FROM OUVRAGE;
- END LOOP;
- CLOSE liste_exemplaire;
+     OPEN liste_exemplaire;
+     FOR LIGNE IN liste_exemplaire LOOP
+       number_uses = utils.number_uses_book(liste_exemplaire.ISBN,liste_exemplaire.Numero_exemplaire);
+       CASE
+         WHEN number_uses<11 THEN UPDATE EXEMPLAIRE SET Etat = ''NEUF'' WHERE ISBN = liste_exemplaire.ISBN AND Numero_exemplaire = liste_exemplaire.Numero_exemplaire;
+         WHEN number_uses>11 AND number_uses<25 THEN UPDATE EXEMPLAIRE SET Etat = ''BON'' WHERE ISBN = liste_exemplaire.ISBN AND Numero_exemplaire = liste_exemplaire.Numero_exemplaire;
+         WHEN number_uses>25 AND number_uses<60 THEN UPDATE EXEMPLAIRE SET Etat = ''MOYEN'' WHERE ISBN = liste_exemplaire.ISBN AND Numero_exemplaire = liste_exemplaire.Numero_exemplaire;
+         WHEN number_uses>60 THEN DELETE EXEMPLAIRE WHERE ISBN = liste_exemplaire.ISBN AND Numero_exemplaire = liste_exemplaire.Numero_exemplaire;
+         ELSE ''12''
+     END as liste_exemplaire
+     FROM OUVRAGE;
+     END LOOP;
+     CLOSE liste_exemplaire;
  END;
 /
+/*@langage_de_definition_de_donnees/update_book_quality.sql;
+*/
 /*
 BEGIN
 OPEN liste_exemplaire;
