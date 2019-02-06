beae2eadabe95522ef2e58bb59d10f5940fa7271
JonathanCruard <jonathan.cruard@gmail.com>
2019-02-06 09:41:43 +0100

    add 10,11,17,18

--- langage_de_definition_de_donnees/partie2-2.sql
+++ langage_de_definition_de_donnees/partie2-2.sql
@@ -11,7 +11,7 @@ update emprunts
 set Etat_emprunt = 'RE'
 where Id_emprunt not in (select Id_emprunt
 from details
-where date_retour IS NULL)
+where date_retour IS NULL);
 
 
 -- II - 9
@@ -21,4 +21,20 @@ select titre from ouvrage;
 select ISBN, ID_membre
 from details join (select ID_membre, ID_emprunt from emprunts
 where Etat_emprunt = 'EC' and cree_le < sysdate-14) test
-on details.ID_emprunt = test.ID_emprunt
+on details.ID_emprunt = test.ID_emprunt;
+
+-- II - 11
+select genre, count(*) as nbr_ouvrage
+from ouvrage
+group by genre
+
+-- II - 17
+CREATE or replace view nbr_emprunt as
+select ISBN, count(*)
+from details
+group by ISBN;
+
+-- II - 18
+select *
+from membre
+order by nom, prenom asc;
