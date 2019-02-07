d4f5c19df0cda9282a2bc430d91d873e21f1b70d
francois.courtin.l <francois.courtin1@etu.univ-nantes.fr>
2019-02-07 10:32:52 +0100

    add Q6 partie 6

--- langage_de_definition_de_donnees/partie6.sql
+++ langage_de_definition_de_donnees/partie6.sql
@@ -34,3 +34,32 @@ BEGIN
   RAISE_APPLICATION_ERROR(-20003, 'Impossible de modifier la référence d''un ouvrage emprunté, il faut le rendre puis effectuer une nouvelle location.');
 END;
 /
+
+--TODO Q5
+
+-- Partie VI Q6 --TODO A TESTER
+CREATE OR REPLACE TRIGGER check_expl AFTER DELETE ON Details
+FOR EACH ROW
+
+DECLARE
+  v_date_crea Emprunts.Cree_le%TYPE;
+  v_date_emprunt Exemplaire.Datecalculemprunt%TYPE;
+
+BEGIN
+   SELECT Cree_le INTO v_date_crea
+   FROM Emprunts
+   WHERE Emprunts.Id_emprunt = :old.Id_emprunt;
+
+   SELECT Datecalculemprunt INTO v_date_emprunt
+   FROM Exemplaire
+   WHERE Exemplaire.Isbn = :old.Isbn
+   AND Exemplaire.Numero_exemplaire = :old.Numero_exemplaire;
+
+   IF (v_date_emprunt < v_date_crea) THEN
+      UPDATE Exemplaire
+      SET Nombre_emprunts = Nombre_emprunts + 1
+      WHERE Exemplaire.Isbn = :old.Isbn
+      AND Exemplaire.Numero_exemplaire = :old.Numero_exemplaire;
+   END IF;
+END;
+/
