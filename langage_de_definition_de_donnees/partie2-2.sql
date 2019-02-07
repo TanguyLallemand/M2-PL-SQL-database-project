52d194595e18b208ace16700728c89219f31468f
LouisonF <fresnaislouison@gmail.com>
2019-02-07 16:46:58 +0100

    udate partie 2.2

--- langage_de_definition_de_donnees/partie2-2.sql
+++ langage_de_definition_de_donnees/partie2-2.sql
@@ -1,3 +1,12 @@
+-- II - 1
+--CF partie 2
+
+-- II - 2
+--CF partie 2
+
+-- II - 3
+--CF partie 2
+
 -- II - 4
 SELECT * FROM Ouvrage;
 SELECT * FROM Exemplaire;
@@ -6,12 +15,18 @@ SELECT * FROM Emprunts;
 SELECT * FROM Details;
 SELECT * FROM Genre;
 
+-- II - 5
+--CF partie 1
+
 -- II - 6
 UPDATE Emprunts
 SET Etat_emprunt = 'RE'
 WHERE Id_emprunt NOT IN (SELECT Id_emprunt
 FROM Details
 WHERE Date_retour IS NULL);
+
+-- II - 7
+--CF partie 4 question 6
 
 -- II - 8
 
@@ -29,14 +44,14 @@ ON Details.Id_emprunt = Test.Id_emprunt;
 -- II - 11
 SELECT Genre, Count(*) AS Nbr_ouvrage
 FROM Ouvrage
-GROUP BY Genre
+GROUP BY Genre;
 
 -- II - 12
 SELECT Nom, Prenom, M.Id_membre, Sum(Date_retour-Cree_le)/Count(*) AS Temps_moyen_emprunt
 FROM Emprunts C, Details T, Membre M
 WHERE C.Id_emprunt = T.Id_emprunt AND M.Id_membre = C.Id_membre
 GROUP BY M.Id_membre, M.Nom, M.Prenom
-ORDER BY Id_membre
+ORDER BY Id_membre;
 
 -- II - 13
 SELECT Genre, Sum(Date_retour-Cree_le)/Count(*)
@@ -45,24 +60,17 @@ WHERE Emprunts.Id_emprunt = Details.Id_emprunt AND Ouvrage.Isbn = Details.Isbn
 GROUP BY Ouvrage.Genre;
 
 -- II - 14
-SELECT Count(Id_emprunt) AS Nbr_pret, Id_emprunt FROM Details
-WHERE Round(Months_between(Sysdate,Date_retour))<='12'
-GROUP BY Id_emprunt
-HAVING Count(Id_emprunt) > 1
-ORDER BY Id_emprunt;
--- en cours
-SELECT Details.Isbn, Ouvrage.Titre
+SELECT Details.Isbn, Ouvrage.Titre, Count(*) nombre_emprunts
 FROM Details, Ouvrage
 WHERE Round(Months_between(Sysdate,Details.Date_retour))<='12' AND Ouvrage.Isbn = Details.Isbn
-GROUP BY Details.Isbn
-HAVING Count(*) > 4
+GROUP BY Details.Isbn, Ouvrage.Titre
+HAVING Count(*) > 10;
 
 -- II - 15
 SELECT O.Isbn, E.Numero_exemplaire
 FROM Ouvrage O, Exemplaire E
 WHERE O.Isbn = E.Isbn
 ORDER BY O.Isbn;
--- fait plus ou moins le job, on pourrait essayer d'avoir les numeros d'exemplaire dans la même case.
 
 -- II - 16
 CREATE OR REPLACE VIEW Nbr_ouvrage AS
@@ -115,5 +123,3 @@ END;
 -- II - 20
 SELECT * FROM Genre;
 SELECT O.Genre, O.Titre FROM Ouvrage O ORDER BY O.Genre, O.Titre;
-
---Ca reponds à la question en vrai !
