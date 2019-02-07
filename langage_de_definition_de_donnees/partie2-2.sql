927a7139c8bf72873e62a3b7943705caf1473022
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-02-07 16:27:01 +0100

    beautify it

--- langage_de_definition_de_donnees/partie2-2.sql
+++ langage_de_definition_de_donnees/partie2-2.sql
@@ -7,110 +7,110 @@ SELECT * FROM Details;
 SELECT * FROM Genre;
 
 -- II - 6
-update emprunts
-set Etat_emprunt = 'RE'
-where Id_emprunt not in (select Id_emprunt
-from details
-where date_retour IS NULL);
+UPDATE Emprunts
+SET Etat_emprunt = 'RE'
+WHERE Id_emprunt NOT IN (SELECT Id_emprunt
+FROM Details
+WHERE Date_retour IS NULL);
 
 
 -- II - 9
-select titre from ouvrage;
+SELECT Titre FROM Ouvrage;
 
 -- II - 10
-select ISBN, ID_membre
-from details join (select ID_membre, ID_emprunt from emprunts
-where Etat_emprunt = 'EC' and cree_le < sysdate-14) test
-on details.ID_emprunt = test.ID_emprunt;
+SELECT Isbn, Id_membre
+FROM Details JOIN (SELECT Id_membre, Id_emprunt FROM Emprunts
+WHERE Etat_emprunt = 'EC' AND Cree_le < Sysdate-14) Test
+ON Details.Id_emprunt = Test.Id_emprunt;
 
 -- II - 11
-select genre, count(*) as nbr_ouvrage
-from ouvrage
-group by genre
+SELECT Genre, Count(*) AS Nbr_ouvrage
+FROM Ouvrage
+GROUP BY Genre
 
 -- II - 12
-SELECT nom, prenom, M.ID_membre, SUM(Date_retour-Cree_le)/COUNT(*) as temps_moyen_emprunt
-FROM emprunts C, details T, membre M
-WHERE C.ID_EMPRUNT = T.ID_EMPRUNT and M.ID_membre = C.ID_membre
-group by M.ID_membre, M.nom, M.prenom
-order by ID_membre
+SELECT Nom, Prenom, M.Id_membre, Sum(Date_retour-Cree_le)/Count(*) AS Temps_moyen_emprunt
+FROM Emprunts C, Details T, Membre M
+WHERE C.Id_emprunt = T.Id_emprunt AND M.Id_membre = C.Id_membre
+GROUP BY M.Id_membre, M.Nom, M.Prenom
+ORDER BY Id_membre
 
 -- II - 13
-select genre, SUM(Date_retour-Cree_le)/COUNT(*)
-from emprunts, details, ouvrage
-where emprunts.ID_EMPRUNT = details.ID_EMPRUNT and ouvrage.ISBN = details.ISBN
-group by ouvrage.genre;
+SELECT Genre, Sum(Date_retour-Cree_le)/Count(*)
+FROM Emprunts, Details, Ouvrage
+WHERE Emprunts.Id_emprunt = Details.Id_emprunt AND Ouvrage.Isbn = Details.Isbn
+GROUP BY Ouvrage.Genre;
 
 -- II - 14
-SELECT COUNT(ID_EMPRUNT) AS nbr_pret, ID_EMPRUNT FROM details
-WHERE ROUND(MONTHS_BETWEEN(SYSDATE,date_retour))<='12'
-GROUP BY ID_EMPRUNT
-HAVING COUNT(ID_EMPRUNT) > 1
-ORDER BY ID_EMPRUNT;
+SELECT Count(Id_emprunt) AS Nbr_pret, Id_emprunt FROM Details
+WHERE Round(Months_between(Sysdate,Date_retour))<='12'
+GROUP BY Id_emprunt
+HAVING Count(Id_emprunt) > 1
+ORDER BY Id_emprunt;
 -- en cours
-select details.isbn, ouvrage.titre
-from details, ouvrage
-WHERE ROUND(MONTHS_BETWEEN(SYSDATE,details.date_retour))<='12' and ouvrage.isbn = details.isbn
-group by details.isbn
-HAVING COUNT(*) > 4
+SELECT Details.Isbn, Ouvrage.Titre
+FROM Details, Ouvrage
+WHERE Round(Months_between(Sysdate,Details.Date_retour))<='12' AND Ouvrage.Isbn = Details.Isbn
+GROUP BY Details.Isbn
+HAVING Count(*) > 4
 
 -- II - 15
-SELECT O.ISBN, E.Numero_exemplaire
-FROM ouvrage O, exemplaire E
-WHERE O.ISBN = E.ISBN
-ORDER BY O.ISBN;
+SELECT O.Isbn, E.Numero_exemplaire
+FROM Ouvrage O, Exemplaire E
+WHERE O.Isbn = E.Isbn
+ORDER BY O.Isbn;
 -- fait plus ou moins le job, on pourrait essayer d'avoir les numeros d'exemplaire dans la même case.
 
 -- II - 16
-CREATE OR REPLACE VIEW nbr_ouvrage AS
-SELECT E.ID_membre, count(E.ID_emprunt) as nbr_emprunt
-FROM emprunts E, details D
-WHERE E.ID_emprunt = D.ID_emprunt AND D.Date_retour is NULL
-GROUP BY E.ID_membre;
+CREATE OR REPLACE VIEW Nbr_ouvrage AS
+SELECT E.Id_membre, Count(E.Id_emprunt) AS Nbr_emprunt
+FROM Emprunts E, Details D
+WHERE E.Id_emprunt = D.Id_emprunt AND D.Date_retour IS NULL
+GROUP BY E.Id_membre;
 
 -- II - 17
-CREATE or replace view nbr_emprunt as
-select ISBN, count(*) as nbr_emprunt_en_cours
-from details
-group by ISBN;
+CREATE OR REPLACE VIEW Nbr_emprunt AS
+SELECT Isbn, Count(*) AS Nbr_emprunt_en_cours
+FROM Details
+GROUP BY Isbn;
 
 -- II - 18
-select *
-from membre
-order by nom, prenom asc;
+SELECT *
+FROM Membre
+ORDER BY Nom, Prenom ASC;
 
 -- II - 19
 
-create global temporary table emprunts_titre(
-ISBN char(13),
-exemplaire number,
-nbr_emprunts_exemplaire number,
-nbr_emprunts_ouvrage number)
-on commit preserve rows;
+CREATE GLOBAL TEMPORARY TABLE Emprunts_titre(
+Isbn Char(13),
+Exemplaire number,
+Nbr_emprunts_exemplaire number,
+Nbr_emprunts_ouvrage number)
+ON COMMIT PRESERVE ROWS;
 
-insert into emprunts_titre(
-isbn, exemplaire, nbr_emprunts_exemplaire)
-select isbn, Numero_exemplaire, count(*)
-from details
-group by isbn, Numero_exemplaire;
+INSERT INTO Emprunts_titre(
+Isbn, Exemplaire, Nbr_emprunts_exemplaire)
+SELECT Isbn, Numero_exemplaire, Count(*)
+FROM Details
+GROUP BY Isbn, Numero_exemplaire;
 
 DECLARE
 CURSOR C_emprunts_ouvrage IS
-select isbn,SUM(emprunts_titre.nbr_emprunts_exemplaire) emprunts_ouvrage
-from emprunts_titre
-group by isbn;
-nbr_emprunts_cursor C_emprunts_ouvrage%Rowtype;
+SELECT Isbn,Sum(Emprunts_titre.Nbr_emprunts_exemplaire) Emprunts_ouvrage
+FROM Emprunts_titre
+GROUP BY Isbn;
+Nbr_emprunts_cursor C_emprunts_ouvrage%Rowtype;
 
 BEGIN
-FOR ligne IN C_emprunts_ouvrage
+FOR Ligne IN C_emprunts_ouvrage
     LOOP
-    update emprunts_titre set nbr_emprunts_ouvrage=ligne.emprunts_ouvrage where emprunts_titre.isbn=ligne.isbn;
+    UPDATE Emprunts_titre SET Nbr_emprunts_ouvrage=Ligne.Emprunts_ouvrage WHERE Emprunts_titre.Isbn=Ligne.Isbn;
     END LOOP;
 END;
 /
 
 -- II - 20
-select * from genre;
-select o.genre, o.titre from ouvrage o order by o.genre, o.titre;
+SELECT * FROM Genre;
+SELECT O.Genre, O.Titre FROM Ouvrage O ORDER BY O.Genre, O.Titre;
 
---Ca reponds à la question en vrai ! 
+--Ca reponds à la question en vrai !
