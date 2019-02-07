3db14915d393dfb65851d8593de32d280b887261
Louison Fresnais <fresnaislouison@gmail.com>
2019-02-07 09:44:53 +0100

    update question 2.2

--- langage_de_definition_de_donnees/partie2-2.sql
+++ langage_de_definition_de_donnees/partie2-2.sql
@@ -28,6 +28,39 @@ select genre, count(*) as nbr_ouvrage
 from ouvrage
 group by genre
 
+-- II - 12
+SELECT SUM(Date_retour-Cree_le)/COUNT(C.ID_EMPRUNT)
+FROM emprunts C, details T
+WHERE C.ID_EMPRUNT = T.ID_EMPRUNT;
+
+-- II - 13
+SELECT SUM(Date_retour-Cree_le)/COUNT(C.ID_EMPRUNT)
+FROM emprunts C, details T, genre G
+WHERE C.ID_EMPRUNT = T.ID_EMPRUNT AND G.CODE == 'REC'; //TODO Faut-il prevoir une automatisation de la requete sur chacun des genres ?
+
+-- II - 14
+SELECT COUNT(ID_EMPRUNT) AS nbr_pret, ID_EMPRUNT FROM details
+WHERE ROUND(MONTHS_BETWEEN(SYSDATE,date_retour))<='12'
+GROUP BY ID_EMPRUNT
+HAVING COUNT(ID_EMPRUNT) > 1
+ORDER BY ID_EMPRUNT;
+
+-- II - 15
+SELECT O.ISBN, E.Numero_exemplaire
+FROM ouvrage O, exemplaire E
+WHERE O.ISBN = E.ISBN
+ORDER BY O.ISBN;
+-- fait plus ou moins le job, on pourrait essayer d'avoir les numeros d'exemplaire dans la même case.
+
+-- II - 16
+CREATE OR REPLACE VIEW nbr_ouvrage AS
+SELECT E.ID_membre, count(E.ID_emprunt) as nbr_emprunt
+FROM emprunts E, details D
+WHERE E.ID_emprunt = D.ID_emprunt AND D.Date_retour is NULL
+GROUP BY E.ID_membre;
+
+--Vue crée, comment on l'active ?
+
 -- II - 17
 CREATE or replace view nbr_emprunt as
 select ISBN, count(*)
@@ -38,3 +71,10 @@ group by ISBN;
 select *
 from membre
 order by nom, prenom asc;
+
+-- II - 19
+
+-- II - 20
+select Libelle
+from genre;
+--TODO
