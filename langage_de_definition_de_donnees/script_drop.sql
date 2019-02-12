78b2296df5e8e546b9b4ec23a1d8c6393c5f544d
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-02-12 17:33:10 +0100

    add drop views

--- langage_de_definition_de_donnees/script_drop.sql
+++ langage_de_definition_de_donnees/script_drop.sql
@@ -1,16 +1,41 @@
+--------------------------------------------------------------------------------
+-- Delete tables
+--------------------------------------------------------------------------------
 DROP TABLE Ouvrage CASCADE CONSTRAINTS;
 DROP TABLE Exemplaire CASCADE CONSTRAINTS;
 DROP TABLE Membre CASCADE CONSTRAINTS;
 DROP TABLE Emprunts CASCADE CONSTRAINTS;
 DROP TABLE Genre CASCADE CONSTRAINTS;
 DROP TABLE Details CASCADE CONSTRAINTS;
-DROP SYNONYM Abonnes;
-DROP SEQUENCE Uniq_id_membre;
-DROP SEQUENCE Uniq_id_emprunt;
--- Delte datas from recycle bin
+--------------------------------------------------------------------------------
+-- Delete datas from recycle bin
+--------------------------------------------------------------------------------
 Purge TABLE Ouvrage;
 Purge TABLE Exemplaire;
 Purge TABLE Membre;
 Purge TABLE Emprunts;
 Purge TABLE Genre;
 Purge TABLE Details;
+--------------------------------------------------------------------------------
+-- Delete views
+--------------------------------------------------------------------------------
+DROP VIEW Ouvrage_emprunte_depuis_2_sem;
+DROP VIEW Nbr_ouvrage;
+DROP VIEW Nbr_ouvrage_par_genre;
+DROP VIEW Nbr_emprunt;
+DROP VIEW Membre_ordre_alpha;
+DROP VIEW Ouvrages_par_genre;
+DROP VIEW Temps_moyen_emprunt_membre;
+DROP VIEW Temps_moyen_emprunt_genre;
+DROP VIEW Liste_des_ouvrages;
+DROP VIEW Ouvrage_les_plus_populaire_12mois;
+DROP VIEW Ouvrage_les_moins_populaire_3mois;
+DROP VIEW Ouvrage_sans_neuf;
+DROP VIEW Ouvrage_avec_public;
+DROP VIEW Abondance_ouvrages;
+--------------------------------------------------------------------------------
+-- Delete few things to completly purge project
+--------------------------------------------------------------------------------
+DROP SYNONYM Abonnes;
+DROP SEQUENCE Uniq_id_membre;
+DROP SEQUENCE Uniq_id_emprunt;
