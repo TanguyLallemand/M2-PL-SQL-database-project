8b0711efe593101a92e56bb780ad30a6cd6a1054
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-02-12 18:08:35 +0100

    add a procedure to search giving him a regex

--- langage_de_definition_de_donnees/package_infos.sql
+++ langage_de_definition_de_donnees/package_infos.sql
@@ -1,6 +1,6 @@
 CREATE PACKAGE Infos AS
     PROCEDURE Lister_ouvrages;
-
+    PROCEDURE recherche_pattern (pattern_recherche IN VARCHAR2);
 
 END Infos;
 /
@@ -17,6 +17,19 @@ BEGIN
         Dbms_output.Put_line(V_liste_titres.Titre);
     END LOOP;
 END;
+
+PROCEDURE recherche_pattern (pattern_recherche IN VARCHAR2) AS
+CURSOR C_liste_titres IS
+    SELECT Titre
+    FROM Ouvrage
+    WHERE Titre LIKE pattern_recherche;
+V_liste_titres C_liste_titres%Rowtype;
+  BEGIN
+    Dbms_output.Put_line('Liste des ouvrages disponibles :');
+    FOR V_liste_titres IN C_liste_titres LOOP
+        Dbms_output.Put_line(V_liste_titres.Titre);
+    END LOOP;
+  END;
 
 END Infos;
 /
